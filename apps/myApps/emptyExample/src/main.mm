53e060a9fd6e1dc4cf419d23db6b87c7eac55fe5
Justin Cuaresma <justincuaresma@gmail.com>
2013-03-11 18:28:54 -0400

    Revert "don't worry about this"
    This reverts commit e0ecc5a049426f35181c2ca8babaeeec8d0e9c95.

--- apps/myApps/emptyExample/src/main.mm
+++ apps/myApps/emptyExample/src/main.mm
@@ -6,14 +6,7 @@
 int main(){
     //ofAppGlutWindow window;
     //window.setGlutDisplayString("rgba double samples>=4");
-    
 	ofSetupOpenGL(1024,768, OF_FULLSCREEN);			// <-------- setup the GL context
     
 	ofRunApp(new testApp);
-    
-    //ofAppiPhoneWindow * iOSWindow = new ofAppiPhoneWindow();  
-    //iOSWindow->enableAntiAliasing(1);  
-    //iOSWindow->enableRetinaSupport();  
-    //ofSetupOpenGL(iOSWindow, 1024,768, OF_FULLSCREEN);  
-    //ofRunApp(new testApp);  
 }
